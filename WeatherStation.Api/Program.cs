c5f5eb1eee13e90148fdbb8bc642d70af4608270
Guille <glexposito@gmail.com>
2025-05-31 20:06:30 +1200

    chore: remove outdated OpenAPI configuration comment in Program.cs

--- WeatherStation.Api/Program.cs
+++ WeatherStation.Api/Program.cs
@@ -3,7 +3,6 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
